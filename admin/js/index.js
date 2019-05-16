27d213f099bd4e2afd4a1a15a809938fff92824d
lihaiwo <249982536@163.com>
2019-05-16 14:17:40 +0800

    数组转换

--- admin/js/index.js
+++ admin/js/index.js
@@ -6,15 +6,52 @@ new Vue({
     visible: false
   },
   methods: {
-    show: function () {
-      this.visible = true;
-    },
     produce: function () {
-      const data = new Uint8Array(Buffer.from('Node.js中文网'));
-      fs.writeFile('文件.txt', data, (err) => {
-        if (err) throw err;
-        console.log('文件已被保存');
-      });
+      fs.promises.readFile('admin/js/source.json','utf8')
+        .then(dataSource => {
+          dataSource = JSON.parse(dataSource);
+          var min = 0;
+          var max = 0;
+          dataSource = dataSource.map(element => {
+            // min = Math.min(min, element.count);
+            // max = Math.max(max, element.count);
+            return {
+              count: 1,
+              geometry: {"type":"Point","coordinates":element.lnglat}
+            }
+          });
+          // dataSource[0].count = 100;
+          console.log(min, max)
+          return new Uint8Array(Buffer.from(JSON.stringify(dataSource)))
+        })
+        .then(data => {
+          return fs.promises.writeFile('文件.txt',data)
+        })
+        .then( () => {
+          console.log('文件已被保存');
+        })
+    },
+    mock: function () {
+     
+      fs.promises.readFile('admin/js/data.json','utf8')
+        .then(dataSource => {
+          dataSource = JSON.parse(dataSource);
+          var min = 0;
+          var max = 0;
+          dataSource.forEach(element => {
+            min = Math.min(min, element.count);
+            max = Math.max(max, element.count);
+            element.count=1;
+          });
+          dataSource[0].count = 100;
+          return new Uint8Array(Buffer.from(JSON.stringify(dataSource)))
+        })
+        .then(data => {
+          return fs.promises.writeFile('文件.txt',data)
+        })
+        .then( () => {
+          console.log('文件已被保存');
+        })
     }
   }
 })
